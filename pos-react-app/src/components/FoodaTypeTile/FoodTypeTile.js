27bdd3a6e98439d64f4486735f88fd72f3a38fab
Malinsha Vithanage <86399536+MaliVitha@users.noreply.github.com>
2023-07-10 16:40:52 +0530

    Color changing with the mouse

--- pos-react-app/src/components/FoodaTypeTile/FoodTypeTile.js
+++ pos-react-app/src/components/FoodaTypeTile/FoodTypeTile.js
@@ -3,8 +3,12 @@ import classes from './FoodTypeTile.module.css';
 import breakfast_image from '../../Assets/breakfast.png';
 
 const FoodTypeTile = () => {
+
+   const mouseEnterHandler = () => {
+    console.log("Mouse entered");
+   }
   return (
-    <div className = {classes.outer}>
+    <div className = {classes.outer} onMouseEnter={mouseEnterHandler}>
       <img className={classes.image} src= {breakfast_image} alt=''/>
      <div className={classes.titleHolder}>
      <span className={classes.mainTitle} >Breakfast</span>
