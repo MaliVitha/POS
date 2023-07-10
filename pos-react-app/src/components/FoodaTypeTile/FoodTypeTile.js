89ccea2d6b14f020630ad45a565d1e371d5f63a3
Malinsha Vithanage <86399536+MaliVitha@users.noreply.github.com>
2023-07-10 18:04:00 +0530

    Mouse Events done

--- pos-react-app/src/components/FoodaTypeTile/FoodTypeTile.js
+++ pos-react-app/src/components/FoodaTypeTile/FoodTypeTile.js
@@ -6,12 +6,20 @@ const FoodTypeTile = () => {
 
    const mouseEnterHandler = () => {
     console.log("Mouse entered");
-   }
+   };
+
+   const mouseLeaveHandler = () => {
+    console.log("Mouse Leaved");
+   };
+
   return (
-    <div className = {classes.outer} onMouseEnter={mouseEnterHandler}>
+    <div className = {classes.outer} 
+    onMouseEnter={mouseEnterHandler}
+    onMouseLeave={mouseLeaveHandler}
+    >
       <img className={classes.image} src= {breakfast_image} alt=''/>
      <div className={classes.titleHolder}>
-     <span className={classes.mainTitle} >Breakfast</span>
+     <span className={classes.mainTitle} >Breakfast </span>
       <span className={classes.subTitle}>13 items</span>
      </div>
       </div>
