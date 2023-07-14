eebe92c5a3fd2ff6de1b6b243830a249ae7a4ae1
Malinsha Vithanage <86399536+MaliVitha@users.noreply.github.com>
2023-07-14 20:53:01 +0530

    orders completed

--- pos-react-app/src/components/CustomerDetails/CustomerDetails.js
+++ pos-react-app/src/components/CustomerDetails/CustomerDetails.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import classes from './CustomerDetails.module.css';
+import Order from './Order';
 
 const CustomerDetails = () => {
   return (
     <div className={classes.outer}>
       <span className={classes.header}> Customer Name</span>
       <div className={classes.titleHolder}>
-      <span className={classes.mainTitle}>Cashier id : </span>
-      <span className={classes.subTitle}>C008</span>
+        <span className={classes.mainTitle}>Cashier id : </span>
+        <span className={classes.subTitle}>C008</span>
       </div>
+      <div>
+      <Order/>
+      <Order/>
+      <Order/>
+      <Order/>
+      <Order/>
+      <Order/>
+      <Order/>
+      </div>
+     
+
       
     </div>
   )
